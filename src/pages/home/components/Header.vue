3967f89ef40da05c367c68c93dbf91470dc34e71
blueskyxxy <2453775685@qq.com>
2018-09-20 15:31:16 +0800

    '添加swiper'

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/经典/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/经典/游玩主题
+    </div>
+    <div class="header-right">
+      城市<span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -13,25 +20,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header
     display: flex
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
-      height: 0.64rem
-      line-height: 0.64rem
-      margin-top: 0.12rem
-      margin-left: 0.2rem
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
       background: #fff
-      border-radius: 0.1rem
+      border-radius: .1rem
       color: #ccc
     .header-right
       width: 1.24rem
       float: right
       text-align: center
+      .arrow-icon
+        font-size: .24rem
 </style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/28/3e5b3d0e6b72f802.jpg_890x330_c8135dfc.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/38/b2118fb904e59f02.jpg_890x330_4e350bc5.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active  // 进行穿透 这样.wrapper下的组件也能生效了
+      background: #fff
+  .wrapper >>> .swiper-pagination-bullets
+      bottom: .06rem
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom:37.08%;  //图片宽高比
+    background: #eee
+    .swiper
+      width: 100%
+</style>
